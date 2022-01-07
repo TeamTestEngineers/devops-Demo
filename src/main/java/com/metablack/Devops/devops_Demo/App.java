5a3b380d2d848a05bf445f3f254005c7da7bc8b1
TeamTestEngineers <numerouno51@gmail.com>
2022-01-07 18:51:06 +0530

    Maven initial commit

---
+++ src/main/java/com/metablack/Devops/devops_Demo/App.java
@@ -0,0 +1,13 @@
+package com.metablack.Devops.devops_Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
